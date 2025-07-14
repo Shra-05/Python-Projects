92ac83c1a82c026c7da9cfbbc74ef6c32bd55d19
Shra-05 <shravanipol05@gmail.com>
2025-07-14 17:06:08 +0530

    Add Robo Speak project: Robo Speaks Window.py

--- Robo Speaks Window.py
+++ Robo Speaks Window.py
@@ -1,14 +1,15 @@
 import win32com.client
-
-print("Hello, Pycharm. Let's Code")
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 name = input("Enter your name : ")
 greeting = f"Hey, let's start {name}"
 print(greeting)
 speaker.Speak(greeting)
 speaker.Speak('Enter what you want me to speak')
-
 while True:
-    text = input("Enter what you want to speak: ")
+    text = input("Enter what you want to speak: \n")
     print(text)
-    speaker.Speak(text)
+    print('Enter q to stop the programme')
+    if text == 'q':
+        exit()
+    else:
+        speaker.Speak(text)
